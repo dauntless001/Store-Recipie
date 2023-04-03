72244fca199b45a09af28785057b197775633c59
dauntless <suntopmultimedia@gmail.com>
2023-04-03 17:29:38 +0100

    replies

--- stave/models.py
+++ stave/models.py
@@ -13,10 +13,14 @@ class Stave(TimeBasedModel):
     content = models.TextField(max_length=280)
     status = models.CharField(max_length=100, choices=choices.Status.choices, 
                               default=choices.Status.Draft)
-    likes = models.ManyToManyField('base.User', null=True, related_name='stave_likes')
+    likes = models.ManyToManyField('base.User', blank=True, related_name='stave_likes')
+    reply = models.ManyToManyField('self', blank=True)
     slug = models.SlugField(default=strings.generate_random_url)
 
     objects = StaveManager()
 
     def __str__(self):
         return f'stave - {self.slug}'
+    
+    def get_likes(self):
+        return self.likes.all()
